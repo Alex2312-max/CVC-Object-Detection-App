d57a6d24bab8dba06cb0cb72e11fbc78cabea3b8
Alex Clefos <aclefos@yahoo.com>
2021-10-03 22:53:11 +0300

    Requirements version specify

--- app/app/settings.py
+++ app/app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1_b43hqlbks%r@28es-d$yi3p%fcio=*=fy48vj!(+z3zq80xf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
